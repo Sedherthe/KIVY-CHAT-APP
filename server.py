55009e2f5de97c37000ab08d5f70cd451b2582f0
sedherthe <siddhartha.soma1@gmail.com>
2019-09-15 12:04:44 +0530

    Buffering and streaming data: Create server for vary lenght message

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import socket
 
+HEADERSIZE = 10
+
     ## Create a socket for server role ##
-# The address family, kind of socket to use. STREAM here.
 try:
+	# The address family, kind of socket to use. STREAM here.
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 	print('Server Socket successfully created')
 except socket.error as err:
 	print(f'Socket creating failed with {err}')
 
-# Bind assigns a local protocol address to a socket.It is a combination of IPv4 + Port (16bit) address.
-s.bind((socket.gethostbyname('localhost'), 1234))
+s.bind((socket.gethostbyname('localhost'), 1234)) # Bind assigns a local protocol address to a socket.It is a combination of IPv4 + Port (16bit) address.
 s.listen(5) # Called by the TCP server.Int indicates max num of connections kernel should queue.
 
 while True:
@@ -17,5 +18,7 @@ while True:
 	clientsocket, address = s.accept()
 	print(f'Connection from {address} established!!')
 	# It is byte stream by default.
-	clientsocket.send(bytes("Welcome to the Server", 'utf-8'))
-	clientsocket.close()
+	msg = "Welcome to the Server"
+	msg = f'{len(msg):<{HEADERSIZE}}' + msg
+	print(f'Message sent is : {msg}')
+	clientsocket.send(bytes(msg, 'utf-8'))
