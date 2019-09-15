5ef62e01fc1d4ec909c675aaf2cf065157d3138c
sedherthe <siddhartha.soma1@gmail.com>
2019-09-15 12:05:11 +0530

    Buffering and streaming data: Cleit for vary length msg

--- client.py
+++ client.py
@@ -1,13 +1,32 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket()
 s.connect((socket.gethostbyname('localhost'), 1234))
 
-full_msg = ''
 while True:
-	msg = s.recv(8)
-	if len(msg) <= 0:
-		break
-	full_msg += msg.decode('utf-8') # The encoding scheme used.
-	print(f'Message received is : {msg}')
-print(f'The message received is : {full_msg}.')
+
+	full_msg = ''
+	new_msg = True
+
+	while True:
+		
+		msg = s.recv(16) # Receive the 16 len strings.
+		msg = msg.decode('utf-8')
+		print(f'Message received is : {msg}')
+
+		if new_msg:
+			print(f'New message lengthgs: {msg[:HEADERSIZE]}')
+			msglen = int(msg[:HEADERSIZE]) # The message length is send in the first 10 bits of the string.
+			new_msg = False
+		
+		full_msg += msg # The encoding scheme used.
+		
+		if len(full_msg) - HEADERSIZE == msglen:
+			print("Full message received")
+			print(full_msg[HEADERSIZE:]) # First headersize bits contain the size of the message at max.
+			new_msg = True
+			full_msg = ''
+		
+	print(f'The Final message received is : {full_msg}.')
